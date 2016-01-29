bb8e49c3303a632b35b331d0d57c2cc97c1dba71
jacobhorne <jacobhorne@gmail.com>
2016-01-29 10:54:06 -0500

    update

--- js/script.js
+++ js/script.js
@@ -1,45 +1,56 @@
-/* js file */
+ /* js file */
 
 (function( window, undefined ){
 
 
-
-
-
-
-
-
-
-
-
-
-
-//Book Object and Algorithm
+//Book Object
 
 var $container = $('#container');
 
 var books = [
 
-//History
+
+
+
+  //History
   { title: 'Nikola Tesla', url: 'http://amzn.to/1OE6YSa', category: 'history', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00CATSONE&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
-  { title: 'Lewis and Clark', url: 'http://amzn.to/1JgbBF9', category: 'history', img:'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B0194QVC7A&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20" ></a><img src="http://ir-na.amazon-adsystem.com/e/ir?t=thoughtsharkc-20&l=as2&o=1&a=B0194QVC7A'},
-  { title: 'Darwin', url: 'http://amzn.to/1P3HBgf', category: 'history', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B008478VE8&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20" ></a><img src="http://ir-na.amazon-adsystem.com/e/ir?t=thoughtsharkc-20&l=as2&o=1&a=B008478VE8' },
+  { title: 'Lewis and Clark', url: 'http://amzn.to/1JgbBF9', category: 'history', img:'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B0194QVC7A&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20'},
+  { title: 'Darwin', url: 'http://amzn.to/1P3HBgf', category: 'history', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B008478VE8&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20'},
   { title: 'Meditations', url: 'http://amzn.to/1OE7RtM', category: 'history', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00IMLL63O&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
   { title: 'The Legend of King Authur', url: 'http://amzn.to/1RyvGsY', category: 'history', img:'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B0083ZMP5C&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20'},
-  { title: 'The Art of War', url: 'http://amzn.to/1P3T5jF', category: 'history', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B0199LTIKI&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20" ></a><img src="http://ir-na.amazon-adsystem.com/e/ir?t=thoughtsharkc-20&l=as2&o=1&a=B0199LTIKI' },
+  { title: 'The Art of War', url: 'http://amzn.to/1P3T5jF', category: 'history', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B0199LTIKI&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20'},
   
 
-//Mystery 
-  { title: 'Once Gone by Blake Pierce', url: 'http://amzn.to/1SLRBgr', category: 'mystery', img: 'http://ir-na.amazon-adsystem.com/e/ir?t=thoughtsharkc-20&l=as2&o=1&a=B00RPM9MJ6' },
-  { title: 'The Grave Man by david archer', url: 'http://amzn.to/1JWO7oL', category: 'mystery', img:'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B014JOV24K&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20" ></a><img src="http://ir-na.amazon-adsystem.com/e/ir?t=thoughtsharkc-20&l=as2&o=1&a=B014JOV24K'},
-  { title: 'Nightmares of Caitlin', url: 'http://amzn.to/1Rywu12', category: 'mystery', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00E7KTEEW&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20" ></a><img src="http://ir-na.amazon-adsystem.com/e/ir?t=thoughtsharkc-20&l=as2&o=1&a=B00E7KTEEW' },
+  //Mystery 
+  { title: 'Once Gone by Blake Pierce', url: 'http://amzn.to/1SLRBgr', category: 'mystery', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B01991O5UG&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
+  { title: 'The Grave Man by david archer', url: 'http://amzn.to/1JWO7oL', category: 'mystery', img:'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B014JOV24K&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20'},
+  { title: 'Nightmares of Caitlin', url: 'http://amzn.to/1Rywu12', category: 'mystery', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00E7KTEEW&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20'},
 
   //Suspence
   { title: 'Squall by Sean Costello', url: 'http://amzn.to/1P3TPpd', category: 'suspence', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00RPM9MJ6&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
   { title: 'Inheritance by Thomas Wymark', url: 'http://amzn.to/1KH75uB', category: 'suspence', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B010T2CZOC&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
   { title: 'Blackwater Lake by Maggie James', url: 'http://amzn.to/1SLUaPR', category: 'suspence', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B015WYIQ4K&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
   { title: 'Miami Requiem by JB turner', url: 'http://amzn.to/1lWlA7o', category: 'suspence', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00H4IG6EA&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
-  { title: 'Dracula by Bram Stoker', url: 'http://amzn.to/1lWlPzb', category: 'suspence', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B0084B5TK8&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
+  { title: 'the war planners by andrew watts', url: 'http://amzn.to/23vGQmp', category: 'suspence', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00RC2GNCI&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
+
+  //technology
+  { title: 'The 27+ Best Apps for Work', url: 'http://amzn.to/23vHa4B', category: 'technology', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B012BT3KOK&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
+  { title: 'Linux: Learn Linux In 2 Hours: Including All Essential Command Lines. The Beginners Choice for the Linux System', url: 'http://amzn.to/23vHxMv', category: 'technology', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00W3HNZWS&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
+  { title: 'Python for Informatics: Exploring Information: Exploring Information', url: 'http://amzn.to/23vHGzE', category: 'technology', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00K0O8HFQ&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
+  { title: 'HTML: QuickStart Guide - Creating an Effective Website (Wordpress, XHTML, JQuery, ASP, Browsers, CSS, Javascript)', url: 'http://amzn.to/23vJvfN', category: 'technology', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B01ACB7OMS&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
+  
+
+  //hobbies
+  { title: 'Tiny Homes: Tiny Homes for Beginners, Join the Tiny House Movement', url: 'http://amzn.to/1nAsXTE', category: 'hobbies', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B01AK3NE22&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
+  { title: 'Adult Coloring Books: Stress Relieving Patterns', url: 'http://amzn.to/23vIjJw', category: 'hobbies', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B01AWLN98G&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
+  { title: 'Homesteading For Beginners: A Homestead Survival Guide for the Backyard & Urban Farmer', url: 'http://amzn.to/1SLYCxW', category: 'hobbies', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00OV8QE5C&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
+
+
+  //education
+  { title: 'GRE: What You Need to Know: An Introduction to the GRE Revised General Test (Kaplan Test Prep)', url: 'http://amzn.to/1nAvxsR', category: 'education', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00A8ME0M4&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
+  // { title: 'Procrastination: Overcome Procrastination, Increase Productivity & Develop Time Management Strategies for Life', url: 'http://amzn.to/1WPIESy', category: 'hobbies', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B012LEV79U&Format=_SL110_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
+  { title: 'How to Beat a Speeding Ticket Book -Fight that Ticket and Win', url: 'http://amzn.to/1PD0jat', category: 'hobbies', img: 'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00HBUIB7Q&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=thoughtsharkc-20' },
+
 ];
 
 
@@ -68,10 +79,14 @@ var populateBooks = function (books) {
   $container.empty();
   
   for (var i = 0; i < books.length; i++) {
+    console.log(books[i].img)
     
     $div = $('<div>', {
-      
-    }); 
+      class: 'book',
+      css: {
+        backgroundImage: 'url(' + books[i].img + ')',
+      }
+    });
 
     $aNewLink = $('<a/>', {
       href: books[i].url,
@@ -79,16 +94,9 @@ var populateBooks = function (books) {
       class: 'alink'
     });
 
-    $img = $('<img/>', {
-      src: books[i].img,
-      class: 'book'
-    });
+    $aNewLink.append($div);
 
-    $div.append($aNewLink);
-
-    $aNewLink.append($img);
-
-    $container.append($div);
+    $container.append($aNewLink);
   }
 
   setTimeout(function () {
